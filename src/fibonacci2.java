105b24954df8c5c7daf3e4c6955efe9400c271c5
Dibran <dibran.ibisi@gmail.com>
2024-12-18 11:45:57 +0100

    1st version of the  test

---
+++ src/fibonacci2.java
@@ -0,0 +1,53 @@
+public class fibonacci2 {
+
+
+    public static void main(String[] args)
+
+    {
+        int a = 1;
+        long runMethodBelow = fibonaccihoPostupnost(a);
+    }
+
+
+    private static int b=1;
+    private static int c=0;
+
+
+    public static long fibonaccihoPostupnost(int a)
+
+    {
+
+        switch (a)
+
+        {
+            case 1:
+                b=1;
+                c=b;
+                break;
+
+            default:
+                c = b ;
+                b = a;
+                break;
+        }
+
+
+        if (a > 100)
+        {
+            return a;
+        }
+
+        else
+        {
+            System.out.println("Králikov= " + a);
+
+            return fibonaccihoPostupnost(a=a+c);
+
+        }
+
+    }
+
+}
+
+
+
