0fad0153f3845058dd19eed48c30bd4193dfc92d
Dibran <dibran.ibisi@gmail.com>
2024-12-18 12:20:42 +0100

    3rd version of the Fibonacci sequence as a recursive method

--- src/fibonacci2.java
+++ src/fibonacci2.java
@@ -1,47 +1,37 @@
 public class fibonacci2 {
 
 
-    public static void main(String[] args)
-
-    {
+    public static void main(String[] args) {
         int a = 1;
         long runMethodBelow = fibonaccihoPostupnost(a);
     }
 
 
-    public static int b=1;
-    public static int c=0;
+    public static int b = 1;
+    public static int c = 0;
 
 
-    public static long fibonaccihoPostupnost(int a)
+    public static long fibonaccihoPostupnost(int a) {
 
-    {
-
-        switch (a)
-
-        {
+        switch (a) {
             case 1:
-                b=1;
-                c=b;
+                b = 1;
+                c = b;
                 break;
 
             default:
-                c = b ;
+                c = b;
                 b = a;
                 break;
         }
 
 
-        if (a > 100)
-        {
+        if (a > 100) {
             return a;
-        }
-
-        else
-        {
+        } else {
             System.out.println("Králikov= " + a);
 
-            return fibonaccihoPostupnost(a=a+c);
+            return fibonaccihoPostupnost(a = a + c);
 
         }
 
